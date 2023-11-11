a8dfcf86a759e5121027504abe0028b5bb38d7e9
Gleb Matlakhov <MatlakhovG@gmail.com>
2023-11-11 16:42:06 +0700

    Validate to annotation

--- src/main/java/com/example/demo/controllers/CharacterFrequencyController.java
+++ src/main/java/com/example/demo/controllers/CharacterFrequencyController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controllers;
 
-import com.example.demo.dto.CharacterFrequencyDTO;
 import com.example.demo.services.CharacterFrequencyService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/TestJobMG")
+@Validated
 public class CharacterFrequencyController {
     private final CharacterFrequencyService characterFrequencyService;
 
@@ -18,8 +23,13 @@ public class CharacterFrequencyController {
     }
 
     @GetMapping("/charInStr")
-    public String getCharacterFrequencies(@RequestParam("inputString") String inputString) {
+    public String getCharacterFrequencies(@Valid @NotBlank @Pattern(regexp = "[a-zA-Zа-яА-Я0-9]+") @RequestParam("inputString") String inputString) {
         return characterFrequencyService.getCharacterFrequencies(inputString);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(ex.getBindingResult().getFieldError().getDefaultMessage());
+    }
 }
--- src/main/java/com/example/demo/services/CharacterFrequencyService.java
+++ src/main/java/com/example/demo/services/CharacterFrequencyService.java
@@ -1,10 +1,6 @@
 package com.example.demo.services;
 
 import com.example.demo.dto.CharacterFrequencyDTO;
-import com.example.demo.exception.InvalidInputException;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -12,23 +8,18 @@ import java.util.stream.Collectors;
 
 @Service
 public class CharacterFrequencyService {
-    public String getCharacterFrequencies(@NotBlank @Pattern(regexp = "[a-zA-Zа-яА-Я0-9]+") String inputString) {
-        try {
-            validateInput(inputString);
+    public String getCharacterFrequencies(String inputString) {
+        Map<Character, Integer> characterFrequencyMap = new TreeMap<>();
 
-            Map<Character, Integer> characterFrequencyMap = new TreeMap<>();
-
-            for (char c : inputString.toCharArray()) {
-                int count = characterFrequencyMap.getOrDefault(c, 0);
-                characterFrequencyMap.put(c, count + 1);
-            }
-            List<CharacterFrequencyDTO> frequencies = characterFrequencyMap.entrySet().stream().map(entry -> new CharacterFrequencyDTO(entry.getKey(), entry.getValue()))
-                    .sorted(Comparator.comparingInt(CharacterFrequencyDTO::getFrequency).reversed())
-                    .collect(Collectors.toList());
-            return this.listStringHandler(frequencies);
-        } catch (InvalidInputException e) {
-            return e.getMessage();
+        for (char c : inputString.toCharArray()) {
+            int count = characterFrequencyMap.getOrDefault(c, 0);
+            characterFrequencyMap.put(c, count + 1);
         }
+        List<CharacterFrequencyDTO> frequencies = characterFrequencyMap.entrySet().stream().map(entry -> new CharacterFrequencyDTO(entry.getKey(), entry.getValue()))
+                .sorted(Comparator.comparingInt(CharacterFrequencyDTO::getFrequency).reversed())
+                .collect(Collectors.toList());
+        return this.listStringHandler(frequencies);
+
     }
 
     private String listStringHandler(List<CharacterFrequencyDTO> frequencies) {
@@ -42,16 +33,4 @@ public class CharacterFrequencyService {
         return result.toString();
     }
 
-    private boolean isValidInput(String inputString) {
-        return inputString.matches("[a-zA-Zа-яА-Я0-9]+");
-    }
-
-    private void validateInput(String inputString) {
-        if (inputString == null || inputString.isEmpty()) {
-            throw new InvalidInputException("Параметр 'inputString' не может быть пустым.");
-        }
-        if (!isValidInput(inputString)) {
-            throw new InvalidInputException("Недопустимые символы во входной строке.");
-        }
-    }
 }
